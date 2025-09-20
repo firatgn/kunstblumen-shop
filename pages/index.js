3059a023a26060a1b9f8c56cec3b8ae9460e5556
firatgn <firatgezirgen@hotmail.com>
2025-09-20 20:26:31 +0200

    Image and titel updated

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,10 @@ export default function Home() {
   return (
     <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
       <header style={{ display: 'flex', alignItems: 'center', marginBottom: '2rem' }}>
-        <Image src="/logo.png" alt="Logo" width={50} height={50} />
-        <h1 style={{ marginLeft: '1rem' }}>Kunstblumen-Shop</h1>
+        <Image src="/Becalla.png" alt="Logo" width={150} height={150} />
+        <h1 style={{ marginLeft: '1rem' }}>Becalla Flowers</h1>
       </header>
-      <p>Willkommen im Kunstblumen-Shop! Hier finden Sie elegante und langlebige Kunstblumen.</p>
-      <section style={{ marginTop: '2rem' }}>
-        <ProductCard 
-          id="flower001"
-          name="Rosenarrangement"
-          price="120.00"
-          description="Elegantes Kunstblumen-Gesteck in Rot."
-          image="/images/rose.jpg"
-        />
-      </section>
+      <p>Çok yakında sizin için buradayız.</p>
     </main>
   );
 }
